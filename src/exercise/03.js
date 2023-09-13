c53ff87c99cb1c10ff8a1bac5583d77bf86e4a56
Masoud <masoud.zeinalabedini@gmail.com>
2023-09-13 13:44:00 +0330

    Finish the first extra credit of third exercise

--- src/exercise/03.js
+++ src/exercise/03.js
@@ -13,6 +13,14 @@ const CountContext = React.createContext()
 //   🐨 return your context provider with the value assigned to that array and forward all the other props
 //   💰 more specifically, we need the children prop forwarded to the context provider
 
+function useCount() {
+  const context = React.useContext(CountContext)
+  if (!context) {
+    throw new Error('useCount must be used within CountProvider')
+  }
+  return context
+}
+
 function CountProvider({children}) {
   const [count, setCount] = React.useState(0)
 
@@ -25,13 +33,13 @@ function CountProvider({children}) {
 
 function CountDisplay() {
   // 🐨 get the count from useContext with the CountContext
-  const {count} = React.useContext(CountContext)
+  const {count} = useCount()
   return <div>{`The current count is ${count}`}</div>
 }
 
 function Counter() {
   // 🐨 get the setCount from useContext with the CountContext
-  const {setCount} = React.useContext(CountContext)
+  const {setCount} = useCount()
   const increment = () => setCount(c => c + 1)
   return <button onClick={increment}>Increment count</button>
 }
